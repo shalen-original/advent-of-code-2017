fb954f04454ea4b4bdfb0fe70baf4dbe4debf9b4
Matteo Nardini <matteo.shalen@gmail.com>
2017-12-08 23:46:29 +0100

    Maybe using the right data structure helps, ah?

--- src/Day5.hs
+++ src/Day5.hs
@@ -2,6 +2,7 @@ module Day5 where
 
 import Data.List
 import Control.DeepSeq
+import Data.Sequence as Seq
 
 run :: IO () 
 run = do
@@ -14,33 +15,27 @@ run = do
     putStrLn . show . part2 . formatInput $ text
     putStrLn ""
 
-formatInput :: String -> [Int]
-formatInput = (map read) . lines 
+formatInput :: String -> Seq Int
+formatInput = Seq.fromList . (map read) . lines 
 
 -- Answer to part one
-part1 :: [Int] -> Int
+part1 :: Seq Int -> Int
 part1 lst = numberOfJumps lst 0 0
 
-numberOfJumps :: [Int] -> Int -> Int -> Int
-numberOfJumps lst currPos acc 
-    | newPos < 0 || newPos >= (length lst) = acc + 1
-    | otherwise = newList `deepseq` (numberOfJumps newList newPos (acc + 1))
-    where newPos = currPos + (lst !! currPos)
-          newList = (inc lst currPos)
-
-inc :: [Int] -> Int -> [Int]
-inc lst currPos = map (\(el, pos) -> if pos == currPos then el + 1 else el)(zip lst [0, 1..])
+numberOfJumps :: Seq Int -> Int -> Int -> Int
+numberOfJumps seq currPos acc 
+    | newPos < 0 || newPos >= (Seq.length seq) = acc + 1
+    | otherwise = numberOfJumps newSeq newPos (acc + 1)
+    where newPos = currPos + (Seq.index seq currPos)
+          newSeq = Seq.adjust (\el -> el + 1) currPos seq
 
 -- Answer to part two
-part2 :: [Int] -> Int
+part2 :: Seq Int -> Int
 part2 lst = numberOfJumps2 lst 0 0
 
-numberOfJumps2 :: [Int] -> Int -> Int -> Int
-numberOfJumps2 lst currPos acc 
-    | newPos < 0 || newPos >= (length lst) = acc + 1
-    | otherwise = newList `deepseq` (numberOfJumps2 newList newPos (acc + 1))
-    where newPos = currPos + (lst !! currPos)
-          newList = (inc2 lst currPos)
-
-inc2 :: [Int] -> Int -> [Int]
-inc2 lst currPos = map (\(el, pos) -> if pos == currPos then (if el >= 3 then el - 1 else el + 1) else el)(zip lst [0, 1..])
+numberOfJumps2 :: Seq Int -> Int -> Int -> Int
+numberOfJumps2 seq currPos acc 
+    | newPos < 0 || newPos >= (Seq.length seq) = acc + 1
+    | otherwise = numberOfJumps2 newSeq newPos (acc + 1)
+    where newPos = currPos + (Seq.index seq currPos)
+          newSeq = Seq.adjust (\el -> if el >= 3 then el - 1 else el + 1) currPos seq
